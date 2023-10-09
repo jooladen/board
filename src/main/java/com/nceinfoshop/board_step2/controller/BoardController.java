cca7e3c498ae5ee570067b55dcc7f2f01038f2f1
jooladen <jooladen@gmail.com>
2023-10-09 21:49:57 +0900

    step2. boardlist

--- src/main/java/com/nceinfoshop/board_step2/controller/BoardController.java
+++ src/main/java/com/nceinfoshop/board_step2/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.nceinfoshop.board_step2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,6 +29,13 @@ public class BoardController {
 	@GetMapping("/")
 	public String main() {
 		return "login";
+	}
+	
+	@GetMapping("/board/list")
+	public String boardList(Model model) {
+		model.addAttribute("list", boardService.boardList());
+		
+		return "boardlist";
 	}
 	
 	@GetMapping("/board/write")
--- src/main/java/com/nceinfoshop/board_step2/service/BoardService.java
+++ src/main/java/com/nceinfoshop/board_step2/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.nceinfoshop.board_step2.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,9 @@ public class BoardService {
 	
 	public void write(Board board) {
 		boardRepository.save(board);
+	}
+	
+	public List<Board> boardList(){
+		return boardRepository.findAll();
 	}
 }
